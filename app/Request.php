6d14e224fe7d82d62945ccd1e4787f4a87754a31
Darrell Cormier <90807553+darcorm@users.noreply.github.com>
2025-02-13 23:44:42 -0400

    refactor request object

--- app/Request.php
+++ app/Request.php
@@ -4,35 +4,40 @@ namespace app;
 
 class Request
 {
-    public array $headers;
+    private array $headers;
 
     public function __construct()
     {
         $this->headers = getallheaders();
     }
 
-    public function isGet()
+    public function isGet(): bool
     {
         return htmlspecialchars($_SERVER['REQUEST_METHOD']) === 'GET' ? true : false;
     }
 
-    public function isPost()
+    public function isPost(): bool
     {
         return htmlspecialchars($_SERVER['REQUEST_METHOD']) === 'POST' ? true : false;
     }
 
-    public function get()
+    public function get(): array
     {
         return filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     }
 
-    public function post()
+    public function post(): array
     {
         return filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     }
 
-    public function getHeader(String $header)
+    public function getHeader(String $header): String
     {
         return array_key_exists($header, $this->headers) ?? $this->headers[$header];
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
     }
 }
